720c62be95d3d2bf5a0adfc5c826aa240d58d568
Thắng Nguyễn <thang.nguyen24@student.passerellesnumeriques.org>
2023-04-21 23:40:35 +0700

    change

--- laravel/app/Http/Controllers/tongController.php
+++ laravel/app/Http/Controllers/tongController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class tongController extends Controller
 {
     public function tinhtong (Request $request) {
+        
         $sum = $request -> soA + $request -> soB;
         return view ('sum', compact('sum'));
     }
