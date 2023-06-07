b77d0e8684c1598cc19b61de30380204781e8cbb
Thắng Nguyễn <thang.nguyen24@student.passerellesnumeriques.org>
2023-06-07 18:05:53 +0700

    update-react-api

---
+++ react-api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ShowProduct from './components/ShowProduct';
+import "bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+    <div className="App">
+      <ShowProduct/>
+    </div>
+  );
+}
+
+export default App;
